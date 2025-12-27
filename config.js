13167cac39121ef8bbf90ecaee0c0316c6cb05ad
omais <omais.com>
2025-12-27 19:13:18 +0500

    24 HOURS

--- config.js
+++ config.js
@@ -19,9 +19,9 @@ module.exports = {
   DEFAULT_TIME_SLOTS: ['08:00', '11:30', '15:00'], // Preferred start times for automatic scheduling
   
   // Working hours
-  WORK_START_HOUR: 8, // 8 AM
-  WORK_END_HOUR: 18, // 6 PM
-  WORKING_DAYS: [1, 2, 3, 4, 5], // 0=Sunday ... 6=Saturday (default: Mon-Fri)
+  WORK_START_HOUR: 0, // 12 AM (midnight)
+  WORK_END_HOUR: 24, // 12 AM (midnight next day)
+  WORKING_DAYS: [0, 1, 2, 3, 4, 5, 6], // All days of the week (Mon-Sun)
   
   // Alternate date suggestion
   MAX_DAYS_TO_SUGGEST: 14, // Look ahead 14 days for alternatives
